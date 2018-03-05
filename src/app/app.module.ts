d1fe5287b042dd6ec3875fdd3d0178f449be656e
Reilly <reilly.wood@icloud.com>
2018-03-05 22:40:36 +0900

    remove whitespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({

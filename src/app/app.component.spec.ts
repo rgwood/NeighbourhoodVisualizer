e60a4ee9efc03f46af3b0657c173f0d2f6046252
Reilly <reilly.wood@icloud.com>
2018-03-05 20:43:25 +0900

    Fix test dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ReactiveFormsModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
